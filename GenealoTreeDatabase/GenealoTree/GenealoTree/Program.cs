ec56e2d26dbf4e4dc1674da68e683bcf6a5643ab
taylor.ruhoff <taylor.ruhoff@ndus.edu>
2018-10-28 20:46:58 -0500

    fixed ppt

--- GenealoTreeDatabase/GenealoTree/GenealoTree/Program.cs
+++ GenealoTreeDatabase/GenealoTree/GenealoTree/Program.cs
@@ -23,7 +23,7 @@ namespace GenealoTree
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new PersonalInformationForm());
+            //Application.Run(new PersonalInformationForm(new Person('a')));
             //Application.Run(new AddModifyForm());
             //Application.Run(new SelectionViewForm());
             Application.Run(new WelcomeScreen());
